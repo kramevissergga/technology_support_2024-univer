03c4bf6ba6984a1047fd7aef304104c44e3195e3
kramevissergga <kramevissergga@gmail.com>
2024-03-21 19:14:10 +0200

    Fix index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,21 +1,21 @@
-const { Github: GithubServiсe } = require('../services/github.service');
+const { Github: GithubService } = require('../services/github.service');
 
 class Github {
  // query {owner,repo, type }
  static async getTopContributors(req, res) {
   try {
    const { owner } = req.query;
-   const { repo } = req.query;
+   const { repos } = req.query;
    const { type } = req.query;
 
    if (!owner || !repos || !type) {
     return res.status(400).json({ err: 'owner, repo , type request field' });
    }
 
-   const response = await GithubService.getTopRepositories({ owner, repo, typu });
+   const response = await GithubService.getTopRepositories({ owner, repos, type });
 
    return res.send(response);
-  } catch (errоr) {
+  } catch (error) {
    return res.status(500).json(error.message);
   }
  }
